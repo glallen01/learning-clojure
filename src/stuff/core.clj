bc936b719cf8842eca23ba4d96572c16dfa6ad1f
George Allen <george@simspace.com>
2016-09-12 12:37:48 -0400

    add gzip reading; basic bro-cut-ish function

--- src/stuff/core.clj
+++ src/stuff/core.clj
@@ -1,13 +1,13 @@
 (ns stuff.core
-  (:gen-class))
-
-(require 
- '[clojure.java.io :as io]
- '[semantic-csv.core :as sc :refer :all]
- '[clojure.string :as str]
- '[clojure.pprint]
- '[clojure-csv.core :as csv])
-
+  (:gen-class)
+  (:require
+   [clojure.java.io :as io]
+   [semantic-csv.core :as sc :refer :all]
+   [clojure.string :as str]
+   [clojure.pprint]
+   [clojure-csv.core :as csv])
+  (:import [java.util.zip GZIPInputStream])
+  )
 
 (defn log-lines [file-name]
   (-> (slurp file-name)
@@ -41,9 +41,13 @@
       last))
 
 (defn bro-fields
-  "returns a vector of fields from the brolog header"
+  "returns a vector of fields from the brolog header (can read .gz if named)"
   [file-name]
-  (with-open [in-file (io/reader file-name)] 
+  (with-open [in-file
+              (if (re-find #"\.gz$" file-name)
+                (io/reader (GZIPInputStream. (io/input-stream file-name)))
+                (io/reader file-name))
+              ]
     (let [lines (take 10 (line-seq in-file))]
       (vec (rest (str/split (apply str (filter
                                         #(re-matches #"^#fields\t.*" %)
@@ -54,14 +58,41 @@
 (defn bro-process-log
   "read log and do stuff"
   [file-name]
-  (with-open [in-file (io/reader file-name)]
+  (with-open [in-file
+              (if (re-find #"\.gz$" file-name)
+                (io/reader (GZIPInputStream. (io/input-stream file-name)))
+                (io/reader file-name))
+              ]
     (->>
      (csv/parse-csv in-file
                   :delimiter \tab)
      remove-comments
      (mappify {:header (bro-fields file-name)})
      doall)))
-  
+
+(defn foo
+  "count IPs"
+  [brolog]
+  (doall (map
+          #(println (str %2 ": " (%1 :uid) "\t" (%1 :id.orig_h)))
+          (bro-process-log "conn.log") (range))))
+
+(defn foo*
+  "count IPs"
+  [brolog]
+  (doall (map
+          #(println (str %2 ": " (%1 :uid) "\t" (%1 :id.orig_h)))
+          (bro-process-log "conn.log") (range))))
+
+(defn bro-cut
+  "print src dst port proto"
+  [brolog & key-list]
+  (map #(select-keys %
+                             (if key-list
+                               key-list
+                               [:id.orig_h :id.orig_p :id.resp_h :id.resp_p]))
+       brolog))
+
 (defn -main
   "do stuff"
   [& args]
@@ -77,3 +108,6 @@
     ;; (with-out-str (clojure.pprint/pprint (foo "conn.log")))
     ;; (doseq #(-> % str println) (foo** "conn.log"))
     ))
+
+;; (def foo (bro-process-log "conn.log"))
+;; (apply map println foo)
